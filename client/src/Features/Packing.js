11f89799b6cbd2f80f53bab71c2fae1a37e50098
Michael Zogin <mdzogin@gmail.com>
2022-03-25 13:49:02 -0400

    packing list 1st draft

--- client/src/Features/Packing.js
+++ client/src/Features/Packing.js
@@ -1,19 +1,53 @@
-import React, {useState} from 'react'
-import PForm from './PForm'
-import PList from './PList'
+import React from 'react';
 
 
-const Packing = () => {
 
+const Budgeting = () => {
+  const [currentInput, setCurrentInput] = React.useState('')
+  const [list, setList] = React.useState([])
+
+  const addItem = event =>{
+    event.preventDefault();
+    const newList = list;
+
+  newList.unshift({content:currentInput,isCompleted:false});
+  setList([...newList]);
+  setCurrentInput("");
+  };
+  const deleteItem = (index) =>{
+    var newList = list;
+    newList.splice(index, 1);
+    setList([...newList]);
+
+  };
     return(
-        <div className='packing'>
-            <p>Packing component</p>
-            <div className='P-content'>
-            <PList />
-            <PForm />
-            </div>
-        </div>
-    )
-}
+        <div>
+        <div className="outer-box">
+          <h1>Packing List</h1>
+          <form className="input-box">
+            <input className="input" onChange={event=> {
+              setCurrentInput(event.target.value);
+            }}
+              value={currentInput}
+            />
+            <button clasName="add-button" onClick={addItem}>+</button>
+            </form>
+            {list.map(({content}, index)=>{
+              return (
+                <div className="packing-container">
+                <div className="del-button" onClick={()=>deleteItem()}>X</div>
+                <div className="packing-main-content">
+                <div className="packing-content">{content}</div>
+                </div>
 
-export default Packing
+                </div>
+              );
+            })}
+            </div>
+            </div>
+            //}
+    );
+};
+//ReactDOM.render(<Budgeting/>, document.getElementById('root'))
+
+export default Budgeting
