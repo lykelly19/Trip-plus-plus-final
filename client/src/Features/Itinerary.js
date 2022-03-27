6ddedf6c86fbd035f6f1606f52fa5216092cd9cc
lykelly19 <lykelly2014@icloud.com>
2022-03-26 20:40:04 -0400

    create modal and stylize Itinerary

--- client/src/Features/Itinerary.js
+++ client/src/Features/Itinerary.js
@@ -1,64 +1,93 @@
 import './Itinerary.css';
-import React from 'react';
+import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
+import Modal from './Modal';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Itinerary = () => {
+
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+ 
+    const openModal = () => {
+        setModalIsOpen(true);
+    }
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    }
+
     return(
         <div>
-            <div class="container itinerary-container py-4 px-5 mt-4">
-                    <table id="" class="table table-striped table-borderless">
-                        <thead>
-                            <tr class="d-flex">
-                                <th class="col-1">#</th>
-                                <th class="col-1">Date</th>
-                                <th class="col-1">Time</th>
-                                <th class="col-2">Event Name</th>
-                                <th class="col-2">Location</th>
-                                <th class="col-5">Notes</th>
-                            </tr>
-                        </thead>
-                        <tbody class="itinerary-items-container">
-                            <tr class="d-flex">
-                                <th class="col-1">#</th>
-                                <th class="col-1">Date</th>
-                                <th class="col-1">Time</th>
-                                <th class="col-2">Event Name</th>
-                                <th class="col-2">Location</th>
-                                <th class="col-5">Notes</th>
-                            </tr>
-                            <tr class="d-flex">
-                                <th class="col-1">#</th>
-                                <th class="col-1">Date</th>
-                                <th class="col-1">Time</th>
-                                <th class="col-2">Event Name</th>
-                                <th class="col-2">Location</th>
-                                <th class="col-5">Notes</th>
-                            </tr>
-                            <tr class="d-flex">
-                                <th class="col-1">#</th>
-                                <th class="col-1">Date</th>
-                                <th class="col-1">Time</th>
-                                <th class="col-2">Event Name</th>
-                                <th class="col-2">Location</th>
-                                <th class="col-5">Notes</th>
-                            </tr>
-                            <tr class="d-flex">
-                                <th class="col-1">#</th>
-                                <th class="col-1">Date</th>
-                                <th class="col-1">Time</th>
-                                <th class="col-2">Event Name</th>
-                                <th class="col-2">Location</th>
-                                <th class="col-5">Notes</th>
-                            </tr>
-                        </tbody>
-                    </table>
+            { modalIsOpen && <Modal modalIsOpen={modalIsOpen} closeModal={closeModal}></Modal> }
+
+            <div className="container itinerary-container py-4 px-5 mt-4">
+                <table id="itinerary-table" className="table table-striped table-borderless">
+                    <thead>
+                        <tr className="d-flex">
+                            <th className="col-1">#</th>
+                            <th className="col-1">Date</th>
+                            <th className="col-1">Time</th>
+                            <th className="col-2">Event Name</th>
+                            <th className="col-2">Location</th>
+                            <th className="col-5">Notes</th>
+                        </tr>
+                    </thead>
+                    <tbody className="itinerary-items-container">
+                        <tr className="d-flex">
+                            <th className="col-1">1</th>
+                            <th className="col-1">2/21/2022</th>
+                            <th className="col-1">7:21 PM</th>
+                            <th className="col-2">Visit Thessaliniki</th>
+                            <th className="col-2">Port of Thessaliniki</th>
+                            <th className="col-5">Bring Camera</th>
+                        </tr>
+                        <tr className="d-flex">
+                            <th className="col-1">2</th>
+                            <th className="col-1">Date</th>
+                            <th className="col-1">Time</th>
+                            <th className="col-2">Event Name</th>
+                            <th className="col-2">Location</th>
+                            <th className="col-5">Notes</th>
+                        </tr>
+                        <tr className="d-flex">
+                            <th className="col-1">3</th>
+                            <th className="col-1">Date</th>
+                            <th className="col-1">Time</th>
+                            <th className="col-2">Event Name</th>
+                            <th className="col-2">Location</th>
+                            <th className="col-5">Notes</th>
+                        </tr>
+                        <tr className="d-flex">
+                            <th className="col-1">4</th>
+                            <th className="col-1">Date</th>
+                            <th className="col-1">Time</th>
+                            <th className="col-2">Event Name</th>
+                            <th className="col-2">Location</th>
+                            <th className="col-5">Notes</th>
+                        </tr>
+                        <tr className="d-flex">
+                            <th className="col-1">5</th>
+                            <th className="col-1">Date</th>
+                            <th className="col-1">Time</th>
+                            <th className="col-2">Event Name</th>
+                            <th className="col-2">Location</th>
+                            <th className="col-5">Notes</th>
+                        </tr>
+                        <tr className="d-flex">
+                            <th className="col-1">6</th>
+                            <th className="col-1">Date</th>
+                            <th className="col-1">Time</th>
+                            <th className="col-2">Event Name</th>
+                            <th className="col-2">Location</th>
+                            <th className="col-5">Notes</th>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
 
-            <Button id="add-button">Add new itinerary item</Button>  
-
-            <div class="container">
-                <h2 class="text-center">Map</h2>
+            <Button id="add-button" onClick={openModal}>Add new itinerary item</Button>  
+            <div className="container">
+                <h2 className="text-center">Map</h2>
             </div>
             
         </div>
---
+++ client/src/Features/Modal.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './Modal.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+const Modal = ({children, modalIsOpen, closeModal}) => {
+    return(
+        <div className="page-background">
+            <div className="modal-stuff">
+                <Button id="close-button" onClick={closeModal}>X</Button>
+                <div className="modal-section" id="first-modal-section">
+                    <p className="modal-heading">Event Name</p>
+                    <input></input>
+                </div>
+                <div className="modal-section">
+                    <p className="modal-heading">Date</p>
+                    <input></input>
+                </div>
+                <div className="modal-section">
+                    <p className="modal-heading">Time</p>
+                    <input></input>
+                </div>
+                <div className="modal-section">
+                    <p className="modal-heading">Location</p>
+                    <input></input>
+                </div>
+                <div className="modal-section">
+                    <p className="modal-heading">Notes</p>
+                    <textarea></textarea>
+                </div>
+                <div className="modal-section">
+                    <Button id="delete-button">Delete</Button>
+                    <Button id="save-button">Save</Button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Modal;;
