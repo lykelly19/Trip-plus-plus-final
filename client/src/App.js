07c01cd07add0a4cf259b31ef891a1c9db94b58d
Jen Yuan Shih <jenyuan_shih@student.uml.edu>
2022-03-26 23:51:18 -0400

    home page styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import logo from './trip-plus-plus.svg'
 
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom";
 
 import Home from './Features/Home';
 import Budgeting from './Features/Budgeting';
@@ -10,34 +10,35 @@ import Packing from './Features/Packing';
 
 function App() {
   return (
-    <Router>
-      <div className="header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button>Logout/in</button>
-      </div>
+    <div id="pageWrapper">
+      <Router>
+        <div className="header">
+          <img src={logo} className="appLogo" alt="logo" />
+          <button id="authButton">Log out</button>
+        </div>
 
-      <nav className="navBar">
-        <Link to="/"> Home </Link>
-        <Link to="/budgeting"> Budgeting </Link>
-        <Link to="/itinerary"> Itinerary </Link>
-        <Link to="/packing"> Packing </Link>
-      </nav>
+        <nav className="navBar">
+          <NavLink className="navLink" to="/"> HOME </NavLink>
+          <NavLink className="navLink" to="/budgeting"> BUDGETING </NavLink>
+          <NavLink className="navLink" to="/itinerary"> ITINERARY </NavLink>
+          <NavLink className="navLink" to="/packing"> PACKING </NavLink>
+        </nav>
 
-      <div className="featureComponents">
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/budgeting" element ={<Budgeting/>}/>
-          <Route path="/itinerary" element ={<Itinerary/>}/>
-          <Route path="/packing" element ={<Packing/>}/>
-        </Routes>
-      </div>
+        <div className="featureComponents">
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/budgeting" element ={<Budgeting/>}/>
+            <Route path="/itinerary" element ={<Itinerary/>}/>
+            <Route path="/packing" element ={<Packing/>}/>
+          </Routes>
+        </div>
 
-      <div className="footer">
-        Footer copy right and stuff
-      </div>
-    </Router>
+        <div className="footer">
+          <p>Copyright text</p>
+        </div>
+      </Router>
+    </div>
   );
 }
 
 export default App;
-
