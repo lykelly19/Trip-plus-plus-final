7b6d26df70abc33631739a868e8c9c3007dab9e4
Jennifer Robles <yenni@Yennis-Mac.fios-router.home>
2022-03-27 21:42:30 -0400

    completed widget & style

--- client/src/Features/Home.js
+++ client/src/Features/Home.js
@@ -3,7 +3,7 @@ export function AddWeather() {
     const script = document.createElement('script');
     script.innerHTML = 
     "!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\'https://weatherwidget.io/js/widget.min.js\';fjs.parentNode.insertBefore(js,fjs);}}(document,\'script\',\'weatherwidget-io-js\');"
-    script.async = true;
+    script.async = false;
     document.body.appendChild(script);
 }
 
@@ -14,6 +14,19 @@ const Home = () => {
 
             <a className="weatherwidget-io" href="https://forecast7.com/en/52d5213d40/berlin/" data-label_1="BERLIN" data-label_2="WEATHER" data-theme="blue-mountains" >BERLIN WEATHER</a>
             
+            {!function (d, s, id) {
+                var js, fjs = d.getElementsByTagName(s)[0];
+                // if (!d.getElementById(id)) {
+                    js = d.createElement(s);
+                    js.id = id;
+                    js.src = 'https:weatherwidget.io/js/widget.min.js';
+                    fjs.parentNode.insertBefore(js, fjs);
+                // }
+            }
+                (document, 'script', 'weatherwidget-io-js')
+            }
+            {/* {AddWeather()} */}
+
             {AddWeather()}
 
         </div>
