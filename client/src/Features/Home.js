e4899bc547917400b83562c079865a2153a6df6f
Jennifer Robles <yenni@Yennis-Mac.fios-router.home>
2022-03-27 21:13:55 -0400

    add weather but bug

--- client/src/Features/Home.js
+++ client/src/Features/Home.js
@@ -1,11 +1,27 @@
 import React from 'react'
+export function AddWeather() {
+    const script = document.createElement('script');
+    script.innerHTML = 
+    "!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\'https://weatherwidget.io/js/widget.min.js\';fjs.parentNode.insertBefore(js,fjs);}}(document,\'script\',\'weatherwidget-io-js\');"
+    script.async = true;
+    document.body.appendChild(script);
+}
 
 const Home = () => {
     return(
         <div>
             <p>Home component</p>
+
+            <a className="weatherwidget-io" href="https://forecast7.com/en/52d5213d40/berlin/" data-label_1="BERLIN" data-label_2="WEATHER" data-theme="blue-mountains" >BERLIN WEATHER</a>
+            
+            {AddWeather()}
+
         </div>
     )
 }
 
 export default Home
+
+
+// raw script
+//!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
