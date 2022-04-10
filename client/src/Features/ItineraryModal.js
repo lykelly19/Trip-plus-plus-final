e7c10c3c3b7469227cc85ce9b186a8f2538377e7
lykelly19 <lykelly2014@icloud.com>
2022-04-09 23:17:28 -0400

    change from functional to class components and fix modal

--- client/src/Features/ItineraryModal.js
+++ client/src/Features/ItineraryModal.js
@@ -1,40 +1,56 @@
-import React from 'react';
+import React, { Component } from "react";
+import { createPortal } from "react-dom";
 import './ItineraryModal.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 
-const Modal = ({children, modalIsOpen, closeModal}) => {
-    return(
-        <div className="page-background">
+const modalStyle = {
+    position: "fixed",
+    left: 0,
+    top: 0,
+    bottom: 0,
+    right: 0,
+    paddingTop: "60px",
+    backgroundColor: "rgba(0,0,0,.2)"
+};
+
+export default class Modal extends Component {
+    render() {
+      return createPortal (
+    
+        <div style={modalStyle}>
+
             <div className="itinerary-modal">
-                <Button id="close-button" onClick={closeModal}>X</Button>
-                <div className="modal-section" id="first-modal-section">
-                    <p className="modal-heading">Event Name</p>
-                    <input></input>
+                    <Button id="close-button" onClick={this.props.closeModal}>X</Button>
+                    <div className="modal-section" id="first-modal-section">
+                        <p className="modal-heading">Event Name</p>
+                        <input></input>
+                    </div>
+                    <div className="modal-section">
+                        <p className="modal-heading">Date</p>
+                        <input></input>
+                    </div>
+                    <div className="modal-section">
+                        <p className="modal-heading">Time</p>
+                        <input></input>
+                    </div>
+                    <div className="modal-section">
+                        <p className="modal-heading">Location</p>
+                        <input></input>
+                    </div>
+                    <div className="modal-section">
+                        <p className="modal-heading">Notes</p>
+                        <textarea></textarea>
+                    </div>
+                    <div className="modal-section">
+                        <Button id="delete-button">Delete</Button>
+                        <Button id="save-button">Save</Button>
+                    </div>
                 </div>
-                <div className="modal-section">
-                    <p className="modal-heading">Date</p>
-                    <input></input>
-                </div>
-                <div className="modal-section">
-                    <p className="modal-heading">Time</p>
-                    <input></input>
-                </div>
-                <div className="modal-section">
-                    <p className="modal-heading">Location</p>
-                    <input></input>
-                </div>
-                <div className="modal-section">
-                    <p className="modal-heading">Notes</p>
-                    <textarea></textarea>
-                </div>
-                <div className="modal-section">
-                    <Button id="delete-button">Delete</Button>
-                    <Button id="save-button">Save</Button>
-                </div>
-            </div>
-        </div>
-    )
+
+            </div>,
+            document.getElementById("modal_root"),
+        );
+    }
 }
 
-export default Modal;;
