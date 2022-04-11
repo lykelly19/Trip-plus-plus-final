ee18e8cd4a497f6c2cd43972b4b3de6009d73dac
Michael Zogin <mdzogin@gmail.com>
2022-04-11 12:27:43 -0400

    new styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,75 @@
-import './App.css';
-import logo from './trip-plus-plus.svg'
+import "./App.css";
+import logo from "./tpp.png";
 import React from "react";
-import {BrowserRouter as Router, Routes, Route, NavLink, useNavigate} from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+  useNavigate,
+} from "react-router-dom";
 
-import Home from './Features/Home';
-import Itinerary from './Features/Itinerary';
-import Packing from './Features/Packing';
-import Budgeting from './Features/Budgeting';
-import Login from './Features/Auth/Login';
-import AuthButton from './Features/Auth/AuthButton';
-import Register from './Features/Auth/Register';
+import Home from "./Features/Home";
+import Itinerary from "./Features/Itinerary";
+import Packing from "./Features/Packing";
+import Budgeting from "./Features/Budgeting";
+import Login from "./Features/Auth/Login";
+import AuthButton from "./Features/Auth/AuthButton";
+import Register from "./Features/Auth/Register";
+
 // import CountView from './Features/CountView';
 
 function App() {
   return (
-    <div id="pageWrapper">
+    <div className="container" id="pageWrapper">
       <Router>
-        <div className="header">
-          <img src={logo} className="appLogo" alt="logo" />
-          <AuthButton/>
-        </div>
-
+        <header className="header">
+          <div className="row d-flex flex-row flex-nowrap justify-content-between align-items-center">
+            <div className="col-4">
+              <img src={logo} className="appLogo" alt="logo" />
+            </div>
+            <div className="col-4 d-flex justify-content-end">
+              <AuthButton />
+            </div>
+          </div>
+        </header>
         <nav className="navBar">
-          <NavLink className="navLink" to="/"> HOME </NavLink>
-          <NavLink className="navLink" to="/budgeting"> BUDGETING </NavLink>
-          <NavLink className="navLink" to="/itinerary"> ITINERARY </NavLink>
-          <NavLink className="navLink" to="/packing"> PACKING </NavLink>
+          <ul className="flex-container">
+            <li>
+              <NavLink className="navLink p-2 box-shadow" to="/">
+                {" "}
+                HOME{" "}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="navLink p-2 box-shadow" to="/budgeting">
+                {" "}
+                BUDGETING{" "}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="navLink p-2 box-shadow" to="/itinerary">
+                {" "}
+                ITINERARY{" "}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className="navLink p-2 box-shadow" to="/packing">
+                {" "}
+                PACKING{" "}
+              </NavLink>
+            </li>
+          </ul>
         </nav>
 
         <div className="featureComponents">
           <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/budgeting" element ={<Budgeting/>}/>
-            <Route path="/itinerary" element ={<Itinerary/>}/>
-            <Route path="/packing" element ={<Packing/>}/>
-            <Route path="/login" element ={<Login/>}/>
-            <Route path="/register" element = {<Register/>}/>
+            <Route path="/" element={<Home />} />
+            <Route path="/budgeting" element={<Budgeting />} />
+            <Route path="/itinerary" element={<Itinerary />} />
+            <Route path="/packing" element={<Packing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
           </Routes>
         </div>
 
