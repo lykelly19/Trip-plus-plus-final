1d1f926dbd4bad61d1447cae0d3e747a3c005137
Michael Zogin <mdzogin@gmail.com>
2022-04-11 22:56:16 -0400

    m

--- client/src/Features/Auth/Login.js
+++ client/src/Features/Auth/Login.js
@@ -1,41 +1,49 @@
 import React, { useState, useEffect } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
-import {useNavigate} from "react-router-dom";
-import { auth } from "../../firebase"
+import { useNavigate } from "react-router-dom";
+import { auth } from "../../firebase";
 
-export default function Login(){
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const routerNavigate = useNavigate();
+import "./Login.css";
 
-    const handleEmailChanges = (e) => {
-        setEmail(e.target.value);
-    }
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const routerNavigate = useNavigate();
 
-    const handlePasswordChanges = (e) => {
-        setPassword(e.target.value)
-    }
+  const handleEmailChanges = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handleSignIn = () =>{
-        signInWithEmailAndPassword(auth, email, password).then(() => {
-            routerNavigate('/')
-        }).catch((err) => {
-            alert(err.message);
-        });
-    }
+  const handlePasswordChanges = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleRegister = () => {
-        routerNavigate('/register')
-    }
+  const handleSignIn = () => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        routerNavigate("/");
+      })
+      .catch((err) => {
+        alert(err.message);
+      });
+  };
 
-    return(
-        <div className="loginPage">
-            <div className = "loginContainer">
-                <input type="email" value={email} onChange={handleEmailChanges}></input>
-                <input type="password" value={password} onChange={handlePasswordChanges}></input>
-                <button onClick={handleSignIn}>Sign In</button>
-                <button onClick={handleRegister}>Create an account</button>
-            </div>
-        </div>
-    )
+  const handleRegister = () => {
+    routerNavigate("/register");
+  };
+
+  return (
+    <div className="loginPage">
+      <div className="loginContainer">
+        <input type="email" value={email} onChange={handleEmailChanges}></input>
+        <input
+          type="password"
+          value={password}
+          onChange={handlePasswordChanges}
+        ></input>
+        <button onClick={handleSignIn}>Sign In</button>
+        <button onClick={handleRegister}>Create an account</button>
+      </div>
+    </div>
+  );
 }
--- client/src/Features/Auth/Register.js
+++ client/src/Features/Auth/Register.js
@@ -1,72 +1,79 @@
 import React, { useState, useEffect } from "react";
-import { auth } from "../../firebase"
-import { createUserWithEmailAndPassword} from "firebase/auth";
+import { auth } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { useNavigate, useHistory } from "react-router-dom";
+import "./Register.css";
 
-export default function Register(){
-    const [userRegisterationInfo, setUserRegisterationInfo] = useState({
-        email: "",
-        password: "",
-        confirmPassword: ""
-    })
+export default function Register() {
+  const [userRegisterationInfo, setUserRegisterationInfo] = useState({
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
 
-    const registerNavigate = useNavigate();
+  const registerNavigate = useNavigate();
 
-    const handleRegister = () => {
-        if(userRegisterationInfo.password !== userRegisterationInfo.confirmPassword){
-            alert("Password entered does not match, please confirm that the password are the same");
-            return;
-        }
-        createUserWithEmailAndPassword(
-            auth, 
-            userRegisterationInfo.email, 
-            userRegisterationInfo.password
-        ).then(() => {
-            registerNavigate("/");
-        }).catch((err) => alert(err.message));
+  const handleRegister = () => {
+    if (
+      userRegisterationInfo.password !== userRegisterationInfo.confirmPassword
+    ) {
+      alert(
+        "Password entered does not match, please confirm that the password are the same"
+      );
+      return;
     }
-
-    const handleBackFromRegister = () => {
-        registerNavigate("/login");
-    }
-
-    return(
-        <div>
-            <input 
-                type="email" 
-                placeholder="Email"
-                value={userRegisterationInfo.email}
-                onChange={(e) => {
-                    setUserRegisterationInfo({
-                        ...userRegisterationInfo,
-                        email: e.target.value
-                    })
-                }}
-            />
-            <input 
-                type="password" 
-                placeholder="Password"
-                value={userRegisterationInfo.password}
-                onChange={(e) => {
-                    setUserRegisterationInfo({
-                        ...userRegisterationInfo,
-                        password: e.target.value
-                    })
-                }}
-            />
-            <input 
-                type="password" 
-                placeholder="Confirm Password"
-                value={userRegisterationInfo.confirmPassword}
-                onChange={(e) => {
-                    setUserRegisterationInfo({
-                        ...userRegisterationInfo,
-                        confirmPassword: e.target.value
-                    })
-                }}
-            />
-            <button onClick={handleRegister}>Register</button>
-            <button onClick={handleBackFromRegister}>Back</button>
-        </div>
+    createUserWithEmailAndPassword(
+      auth,
+      userRegisterationInfo.email,
+      userRegisterationInfo.password
     )
+      .then(() => {
+        registerNavigate("/");
+      })
+      .catch((err) => alert(err.message));
+  };
+
+  const handleBackFromRegister = () => {
+    registerNavigate("/login");
+  };
+
+  return (
+    <div>
+      <input
+        type="email"
+        placeholder="Email"
+        value={userRegisterationInfo.email}
+        onChange={(e) => {
+          setUserRegisterationInfo({
+            ...userRegisterationInfo,
+            email: e.target.value,
+          });
+        }}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={userRegisterationInfo.password}
+        onChange={(e) => {
+          setUserRegisterationInfo({
+            ...userRegisterationInfo,
+            password: e.target.value,
+          });
+        }}
+      />
+      <input
+        type="password"
+        placeholder="Confirm Password"
+        value={userRegisterationInfo.confirmPassword}
+        onChange={(e) => {
+          setUserRegisterationInfo({
+            ...userRegisterationInfo,
+            confirmPassword: e.target.value,
+          });
+        }}
+      />
+      <button onClick={handleRegister}>Register</button>
+      <button onClick={handleBackFromRegister}>Back</button>
+    </div>
+  );
 }
