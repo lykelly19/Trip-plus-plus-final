ee444228231089b437a6b5c2efd7e93ff0c2aa2d
lykelly19 <lykelly2014@icloud.com>
2022-04-12 00:48:02 -0400

    merge for user testing

--- client/src/Features/Home.js
+++ client/src/Features/Home.js
@@ -1,74 +1,99 @@
-import React from 'react';
-import './widget.css';
-import './Home.css';
-import BudgetingPreview from './Budgeting Preview.png';
-import ItineraryPreview from './Itinerary Preview.png';
-import PackingPreview from './Packing Preview.png';
-import BudgetingPreviewChart from './Budget Preview Chart.png';
-
+import React from "react";
+import "./widget.css";
+import "./Home.css";
+import BudgetingPreview from "./Budgeting Preview.png";
+import ItineraryPreview from "./Itinerary Preview.png";
+import PackingPreview from "./Packing Preview.png";
+import BudgetingPreviewChart from "./Budget Preview Chart.png";
 
 export function AddWeather() {
-    const script = document.createElement('script');
-    script.innerHTML = 
-    "!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=\'https://weatherwidget.io/js/widget.min.js\';fjs.parentNode.insertBefore(js,fjs);}}(document,\'script\',\'weatherwidget-io-js\');"
-    script.async = false;
-    document.body.appendChild(script);
+  const script = document.createElement("script");
+  script.innerHTML =
+    "!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');";
+  script.async = false;
+  document.body.appendChild(script);
 }
 
 const Home = () => {
-    return(
-        <div>
-            <p>Home component</p>
-    
-            <div class="weatherDiv">
-                <a className="weatherwidget-io mx-auto" href="https://forecast7.com/en/52d5213d40/berlin/" data-label_1="BERLIN" data-label_2="WEATHER" data-theme="blue-mountains" >BERLIN WEATHER</a>
-
-                {!function (d, s, id) {
-                    var js, fjs = d.getElementsByTagName(s)[0];
-                    js = d.createElement(s);
-                    js.id = id;
-                    js.src = 'https:weatherwidget.io/js/widget.min.js';
-                    fjs.parentNode.insertBefore(js, fjs);
-                }
-                    (document, 'script', 'weatherwidget-io-js')
-                }
-
-                {AddWeather()}
-            </div>
-
-            <div className="mt-5">
-                <div className="featurePreview" id="itineraryPreview">
-                    <div className="previewContent mt-5 mx-4 pt-4">
-                        <p id="upcomingText">Upcoming:</p>
-                        <div className="itineraryText">
-                            <p>2/21/2022 @ 7:21 PM</p>
-                            <p>Visit Thessaliniki</p>
-                        </div>
-                    </div>
-                    <p>Itinerary</p>
-                </div>
-                <div className="featurePreview" id="budgetPreview">
-                    <div className="mt-2 mx-4 pt-3">
-                        <img src={BudgetingPreviewChart} alt="preview budgeting donut chart"></img>
-                    </div>
-                    <p>Budget</p>
-                </div>
-                <div className="featurePreview" id="packingPreview">
-                    <div className="previewContent mt-5 mx-4 pt-5">
-                        <p id="numItems">5</p>
-                        <p>items left to pack</p>
-                    </div>
-                    <p>Packing</p>
-                </div>
-
-            </div>
-
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="greeting col-md-5">
+          <h1 className="display-4">Welcome to Trip++</h1>
+          <p className="lead">make your trip positive</p>
         </div>
-    )
-}
+        <div class="weatherDiv col-md-7">
+          <a
+            className="weatherwidget-io mx-auto"
+            href="https://forecast7.com/en/52d5213d40/berlin/"
+            data-label_1="BERLIN"
+            data-label_2="WEATHER"
+            theme="dark"
+          >
+            BERLIN WEATHER
+          </a>
+
+          {
+            !(function (d, s, id) {
+              var js,
+                fjs = d.getElementsByTagName(s)[0];
+              js = d.createElement(s);
+              js.id = id;
+              js.src = "https:weatherwidget.io/js/widget.min.js";
+              fjs.parentNode.insertBefore(js, fjs);
+            })(document, "script", "weatherwidget-io-js")
+          }
+
+          {AddWeather()}
+        </div>
+      </div>
+      <div className="container widgets">
+        <div className="row mb-3 text-center">
+          <div className="col-md-4 d-flex">
+            <div className="card mb-4 box-shadow mx-2 flex-fill">
+              <div className="card-header">
+                <p className="card-title" id="upcomingText">
+                  Upcoming in your itinerary
+                </p>
+              </div>
+              <div className="card-body">
+                <p className="card-text">2/21/2022 @ 7:21 PM</p>
+                <p className="card-text">Visit Thessaliniki</p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 d-flex">
+            <div className="card mb-4 box-shadow mx-2 flex-fill">
+              <div className="card-header">
+                <p className="card-title">Your budget</p>
+              </div>
+              <div className="card-body">
+                <img
+                  src={BudgetingPreviewChart}
+                  alt="preview budgeting donut chart"
+                ></img>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 d-flex">
+            <div className="card mb-4 box-shadow mx-2 flex-fill">
+              <div className="card-header">
+                <p className="card-title">Items left to pack</p>
+              </div>
+              <div className="card-body">
+                <p className="card-text" id="numItems">
+                  5
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
-
 
 // raw script
 //!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
