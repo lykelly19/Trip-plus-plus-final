8ea776f7befde577a9967b7557dac121c97efb50
Jen Yuan Shih <jenyuan_shih@student.uml.edu>
2022-04-21 16:25:51 -0400

    adding firestore, removing database

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth"
-import { getDatabase } from "firebase/database"
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "@firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,7 +20,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);
 export const auth = getAuth()
+export const db = getFirestore(app);
 
 export default app;
