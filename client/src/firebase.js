96feb8dc8f7b5cc8494f8dc193e4beb8b7041741
Jennifer Robles <yenni@Yennis-Mac.fios-router.home>
2022-04-23 13:43:05 -0400

    changing firebase accout

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -9,13 +9,12 @@ import { getFirestore } from "@firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCbQlcXGi59M4uzYATw4Z6oBsdmLhVRRrQ",
-  authDomain: "trip-plus-plus-5eb42.firebaseapp.com",
-  projectId: "trip-plus-plus-5eb42",
-  storageBucket: "trip-plus-plus-5eb42.appspot.com",
-  messagingSenderId: "498066188188",
-  appId: "1:498066188188:web:e1e1ba3bfe9f22615c8a59",
-  measurementId: "G-S41RSK59XT"
+  apiKey: "AIzaSyBrywFIp_5lGZk6EoYe7jUUud2uaNwIgH4",
+  authDomain: "trip-plus-plus-20a36.firebaseapp.com",
+  projectId: "trip-plus-plus-20a36",
+  storageBucket: "trip-plus-plus-20a36.appspot.com",
+  messagingSenderId: "776132051288",
+  appId: "1:776132051288:web:68c20eba550120bceba52d"
 };
 
 // Initialize Firebase
