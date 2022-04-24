df67e5c4b39b9a9377bd169f796b3255357e356b
lykelly19 <lykelly2014@icloud.com>
2022-04-24 15:33:56 -0400

    on itinerary item edit, set selected to previously saved selected date

--- client/src/Features/TableDatePicker.js
+++ client/src/Features/TableDatePicker.js
@@ -6,12 +6,14 @@ export default function TableDatePicker(itemPrefill) {
     
     const [date, setDate] = useState(null);
     let placeholder = "";
+    let dateObject = null;
     
     if(itemPrefill["selected"] !== "") {
         placeholder = itemPrefill["selected"];
+        dateObject = new Date(placeholder);
     }
 
     return (
-        <DatePicker selected={date} placeholderText={placeholder} onChange={date => setDate(date)} />
+        <DatePicker selected={date ? date : dateObject} onChange={date => setDate(date)} />
     );
 }
