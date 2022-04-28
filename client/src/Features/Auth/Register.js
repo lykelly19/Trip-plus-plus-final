655c230a6950e25002af251193fb884908f10bec
Jennifer Robles <yenni@Yennis-Mac.local>
2022-04-27 21:11:49 -0400

    fixed refresh bug

--- client/src/Features/Auth/Register.js
+++ client/src/Features/Auth/Register.js
@@ -181,4 +181,3 @@ export default function Register() {
     </div>
   );
 }
-git
--- client/src/Features/PackingWidget.js
+++ client/src/Features/PackingWidget.js
@@ -1,27 +1,26 @@
-import React , {useState}from 'react'
+import React , {useState, useEffect}from 'react'
 
 import {readLeftToPack} from './DB/readingfb.js'
 
 
 function PackingWidget() {
 
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState('0');
 
-    
-    const read = () =>{
-   
+  
+    useEffect(() => {
         readLeftToPack().then((data) => {
-            console.log(data);
-            setInput(data);
-        }).catch((error) => {
-            console.log("error")
-        });
+          console.log(data);
+          setInput(data);
+      }).catch((error) => {
+          console.log("error")
+      });
 
-    }
-    
+  });
+  
     
   return (
-    <span onLoad={read()}>{input}</span>
+    <span>{input}</span>
   )
 }
 
